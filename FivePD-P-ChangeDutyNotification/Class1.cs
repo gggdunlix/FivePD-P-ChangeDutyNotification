0766bc2c26e02f29a9ecfe02080037030925b604
GGG Dunlix <jishy710@gmail.com>
2023-05-30 23:02:00 -0400

    still have some json error :(

--- FivePD-P-ChangeDutyNotification/Class1.cs
+++ FivePD-P-ChangeDutyNotification/Class1.cs
@@ -20,25 +20,38 @@ namespace FivePD_P_ChangeDutyNotification
         }
         private async Task OnDutyStatusChange(bool onDuty)
         {
-            var config = API.LoadResourceFile(API.GetCurrentResourceName(), "/config/menu.json");
+            var config = API.LoadResourceFile(API.GetCurrentResourceName(), "/config/callouts.json");
+            Debug.WriteLine("[FivePD Custom Duty Notification] ~r~Got file");
             var json = JObject.Parse(config);
+            Debug.WriteLine("[FivePD Custom Duty Notification] ~r~Got json");
             if (json["DutyNotification"] == null)
             {
-                Debug.WriteLine("[FivePD Custom Duty Notification] ~r~Something is wrong with your menu.json!");
+                Debug.WriteLine("[FivePD Custom Duty Notification] ~r~Something is wrong with your callouts.json!");
                 return;
             }
             JToken notifCfg = json["DutyNotification"];
             if (onDuty)
             {
                 PlayerData plrData = Utilities.GetPlayerData();
+                Debug.WriteLine("[FivePD Custom Duty Notification] ~r~Got data");
                 JToken cfg = json["onDuty"];
-                String text = cfg["text"].ToString().Replace("{name}", plrData.DisplayName).Replace("{callsign}", plrData.Callsign).Replace("{rank}", plrData.Rank).Replace("{department}", plrData.DepartmentShortName);
-                String tooltip = cfg["tooltip"].ToString().Replace("{name}", plrData.DisplayName).Replace("{callsign}", plrData.Callsign).Replace("{rank}", plrData.Rank).Replace("{department}", plrData.DepartmentShortName);
-                String title = cfg["title"].ToString().Replace("{name}", plrData.DisplayName).Replace("{callsign}", plrData.Callsign).Replace("{rank}", plrData.Rank).Replace("{department}", plrData.DepartmentShortName);
+                Debug.WriteLine("[FivePD Custom Duty Notification] ~r~Got cfg");
+                Debug.WriteLine(cfg.ToString());
+                string title = cfg["title"].ToString();
+                Debug.WriteLine("[FivePD Custom Duty Notification] ~r~Got raw title");
+                title.Replace("{name}", plrData.DisplayName).Replace("{callsign}", plrData.Callsign).Replace("{rank}", plrData.Rank).Replace("{department}", plrData.DepartmentShortName);
+                Debug.WriteLine("[FivePD Custom Duty Notification] ~r~title");
+                string tooltip = cfg["tooltip"].ToString();
+                tooltip.Replace("{name}", plrData.DisplayName).Replace("{callsign}", plrData.Callsign).Replace("{rank}", plrData.Rank).Replace("{department}", plrData.DepartmentShortName);
+                Debug.WriteLine("[FivePD Custom Duty Notification] ~r~tooltip");
+                string text = cfg["text"].ToString();
+                text.Replace("{name}", plrData.DisplayName).Replace("{callsign}", plrData.Callsign).Replace("{rank}", plrData.Rank).Replace("{department}", plrData.DepartmentShortName);
+                Debug.WriteLine("[FivePD Custom Duty Notification] ~r~text");
+
                 // The Notification Code was written by Kilo! DevKilo#5880 https://devkilo.webflow.io/
                 Function.Call(Hash.BEGIN_TEXT_COMMAND_THEFEED_POST, "STRING");
                 Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, text);
-                Function.Call(Hash.END_TEXT_COMMAND_THEFEED_POST_MESSAGETEXT, "CHAR_CALL911_1", "CHAR_CALL911_1", false, 0, title, tooltip);
+                Function.Call(Hash.END_TEXT_COMMAND_THEFEED_POST_MESSAGETEXT, "CHAR_CALL911", "CHAR_CALL911", false, 0, title, tooltip);
                 Function.Call(Hash.END_TEXT_COMMAND_THEFEED_POST_TICKER, false, true);
             }
             else
@@ -51,7 +64,7 @@ namespace FivePD_P_ChangeDutyNotification
 
                 Function.Call(Hash.BEGIN_TEXT_COMMAND_THEFEED_POST, "STRING");
                 Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, text);
-                Function.Call(Hash.END_TEXT_COMMAND_THEFEED_POST_MESSAGETEXT, "CHAR_CALL911_1", "CHAR_CALL911_1", false, 0, title, tooltip);
+                Function.Call(Hash.END_TEXT_COMMAND_THEFEED_POST_MESSAGETEXT, "CHAR_CALL911", "CHAR_CALL911", false, 0, title, tooltip);
                 Function.Call(Hash.END_TEXT_COMMAND_THEFEED_POST_TICKER, false, true);
             }
         }
